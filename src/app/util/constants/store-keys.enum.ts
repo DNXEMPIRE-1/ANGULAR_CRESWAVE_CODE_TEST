a3387bccf4e3d9ef632cab03369b4339c9e29980
“Dennis <dennisnzioki019@gmail.com>
2024-05-10 21:39:27 +0300

    Create store-keys.enum.ts

---
+++ src/app/util/constants/store-keys.enum.ts
@@ -0,0 +1,6 @@
+export enum StoreKeys {
+  AccessToken = 'access_token',
+  RefreshToken = 'refresh_token',
+  TimesheetShowList = 'timesheetShowList',
+  TimesheetShowCalendar = 'timesheetShowCalendar',
+}
