834df6e4270946ef24750a3b85d0fc45a0ca0ded
“Dennis <dennisnzioki019@gmail.com>
2024-05-10 21:40:00 +0300

    Create api-endpoints.enum.ts

---
+++ src/app/util/constants/api-endpoints.enum.ts
@@ -0,0 +1,12 @@
+export enum ApiEndpoints {
+  Auth = '/auth',
+  Profiles = '/profiles',
+  ProfileTypes = '/profiles/types',
+  PresetTaskItems = '/profiles/tasks',
+  Timesheets = '/timesheets',
+  Settings = '/settings',
+  MyProfile = '/settings/me',
+  TimesheetMetrics = '/timesheets/metrics',
+  Users = '/users',
+  Roles = '/users/roles',
+}
