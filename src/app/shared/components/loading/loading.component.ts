bd1a45e43863e36bf325dd84ad2cd482a5fa6567
“Dennis <dennisnzioki019@gmail.com>
2024-05-10 21:43:22 +0300

    Create loading.component.ts

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,15 @@
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NgIf, MatProgressSpinnerModule],
+})
+export class LoadingComponent {
+  @Input() size = 100;
+}
